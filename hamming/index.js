63682b70605c10f68bfd60d8dad0e4eae512958f
Jerome Hardaway <jerome@vetswhocode.io>
2017-01-23 12:41:02 -0600

    updated some of the text

--- hamming/index.js
+++ hamming/index.js
@@ -1,16 +1,16 @@
 function Hamming() {
 
     // compute the hamming distance
-    this.compute = function ( stringA, stringB ) {
+    this.compute = function ( stringOne, stringTwo ) {
         var count = 0;
 
         // ensure that related strands are equal in length
-        if( stringA.length !== stringB.length ) {
+        if( stringOne.length !== stringTwo.length ) {
             throw new Error('DNA strands must be of equal length.');
         }
 
-        for( var i = 0; i < stringA.length; i++ ) {
-            if( stringA.charAt(i) !== stringB.charAt(i) ) {
+        for( var i = 0; i < stringOne.length; i++ ) {
+            if( stringOne.charAt(i) !== stringTwo.charAt(i) ) {
                 count++;
             }
         }
